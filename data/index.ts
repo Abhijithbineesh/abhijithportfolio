107ff5d2389c6ef832451cd61301c5295cfe42ff
Abhijithbineesh <abhijith.kpbineesh@gmail.com>
2025-03-18 23:48:11 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,141 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "A software developer from Kerala,India.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My Tech Stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently learning Next Js to build a SAAS application",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Are you interested in collaborating on a project?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "E Commerce Store for a Perfume Store",
+    des: "A full-stack eCommerce platform for perfumes built using the MERN stack (MongoDB, Express.js, React.js, Node.js). ",
+    img: "/1.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "github.com/Abhijithbineesh/perfumeecom",
+  },
+  {
+    id: 2,
+    title: "Freelance - Bamboo product Store",
+    des: "Developed a frontend website for a bamboo product store with a WhatsApp button for direct customer inquiries.",
+    img: "/2.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "vedakala.netlify.app/",
+},
+
+  {
+    id: 3,
+    title: "Youtube Clone",
+    des: "Made a Clone application of Youtube video platform",
+    img: "/3.png",
+    iconLists: ["/re.svg"],
+    link: "github.com/Abhijithbineesh/YoutubeClone",
+  },
+
+  
+ 
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Mern Stack Developer Intern at TechMagi  June 2024 to January 2024",
+    desc: "Assisted in the developing full stack MERN applications",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+
+  {
+    id: 3,
+    title: "Freelance - Bamboo Product Store",
+    desc: "Developed a frontend website for a bamboo product store, showcasing products with a dedicated product page and a WhatsApp button for direct customer inquiries.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg"
+},
+
+  
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"https://github.com/Abhijithbineesh",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link:"https://x.com/abhijithkb138",
+    
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link:"https://www.linkedin.com/in/abhijithkb/"
+
+  },
+];
